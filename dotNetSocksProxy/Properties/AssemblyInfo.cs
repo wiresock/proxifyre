12e8eb5a3165d7049073038dc2d0a38dda7957ba
Vadim Smirnov <vadim@ntkernel.com>
2023-05-30 22:34:28 +0200

    Remove parameters duplication for github actions.

--- dotNetSocksProxy/Properties/AssemblyInfo.cs
+++ dotNetSocksProxy/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("dotNetSocksProxy")]
-[assembly: AssemblyCopyright("Copyright NT Kernel Resources© 2022")]
+[assembly: AssemblyCopyright("Copyright NT KERNEL© 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
